f47edb55031b4ede15e5b6f19eec95567d175ae2
Shogo0908 <yoneshige.sg@gmail.com>
2023-05-26 15:21:09 +0900

    Rubocopの実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-           validates :nickname, presence: true
+  validates :nickname, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
   validates :family_name_kana, presence: true
@@ -17,5 +17,4 @@ class User < ApplicationRecord
 
   # 「全角（カタカナ）」のバリデーションを追加する場合
   validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
- 
- end
+end
