355e41c5fd931e799d8350aa16c9f14c81fb4a35
Shogo0908 <yoneshige.sg@gmail.com>
2023-05-27 19:23:06 +0900

    バリデーションの更新

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,11 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true, numericality: { only_integer: true,  greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :delivery_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_date_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :image, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
