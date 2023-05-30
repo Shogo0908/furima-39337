9c59bf11d14eca4cc1ae5eb3c149fccef5fd27de
Shogo0908 <yoneshige.sg@gmail.com>
2023-05-30 14:20:42 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
   validates :price, presence: true, numericality: { only_integer: true,  greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :description, presence: true
+  validates :description, presence: true, length: { maximum: 1000 }
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank"} 
