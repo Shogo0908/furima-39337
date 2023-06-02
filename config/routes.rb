3196ef5b541fbf11574607bf73f434599c8db512
Shogo0908 <yoneshige.sg@gmail.com>
2023-06-02 15:43:18 +0900

    ルーティング更新

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
 
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
-  resources :items do
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :purchases, only: [:index, :create]
   end
 end
