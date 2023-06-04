39575fac8ec3345c2eb63c30b4e4481c8e624b8e
Shogo0908 <yoneshige.sg@gmail.com>
2023-06-04 13:40:14 +0900

    js鍵の追加

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_deed9b870079e7abaf4ff74d');
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
