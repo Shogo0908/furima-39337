367a75225863978c39c3895a510f412160cfdc69
Shogo0908 <yoneshige.sg@gmail.com>
2023-06-05 16:48:07 +0900

    手数料の計算修正

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -10,11 +10,10 @@ window.addEventListener('load', () => {
     const inputValue = priceInput.value;
 
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-     // 販売手数料（10%）の計算
+    const fee = Math.floor(inputValue * 0.1);
+    addTaxDom.innerHTML = fee; // 販売手数料（10%）の計算
+
     const profitNumber = document.getElementById("profit");
-    const value_result = inputValue * 0.1
-    profitNumber.innerHTML = (Math.floor(inputValue - value_result));
-     // 販売利益の計算
-  })
+    profitNumber.innerHTML = (inputValue - fee); // 販売利益の計算
+  });
 });
